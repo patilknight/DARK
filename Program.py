5d4dc98758dd395163ca4cd28174ddb51c19c8f7
patilknight <118207723+patilknight@users.noreply.github.com>
2023-05-09 21:47:36 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,2 +1,7 @@
 print("HELLO")
-print("Main Ninja Hatauri , me aya pahado se upar ")
+#print("Main Ninja Hatauri , me aya pahado se upar ")
+
+num1 = 5
+num2 = 7
+result = num1 + num2
+print(result)
